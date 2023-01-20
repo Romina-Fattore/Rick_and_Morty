199872bc17c6a92e1417da3d8e976dd52fc7e7c5
Romina <fattorecitter@gmail.com>
2023-01-20 18:10:15 -0300

    Primer commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,16 @@
+
+import style from "./About.module.css";
+
+const About = ()=> {
+    return (
+        <div className={style.container}>
+        <h1>Esta es mi información</h1>
+        <h2>Rick and Morty!</h2>
+        <h3>Cohorte FT-34</h3>
+        <p> Mi nombre es Romina Carla Fattore y esta es la primer app hecha por mi</p>
+        <img src="https://th.bing.com/th/id/OIP.jIu0F4xRwb52oimRbYOLJAHaEK?pid=ImgDet&rs=1" alt=""></img>
+        
+        </div>
+    )
+}
+export default About;
